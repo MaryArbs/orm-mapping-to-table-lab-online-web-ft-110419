5933db6a8a167a5a5969871b1354d1eabc34b19e
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-12-13 22:07:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,7 +7,9 @@ class Student
     @age = age
     @id = id
   end
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+
 
 end
+
+# Remember, you can access your database connection anywhere in this class
+#  with DB[:conn]

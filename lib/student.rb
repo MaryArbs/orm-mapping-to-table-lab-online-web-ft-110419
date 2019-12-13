1ee2ef4f053fbcd8fe33c604e9ca4e5757c738fc
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-12-13 22:09:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,10 +9,10 @@ class Student
   end
 
   def self.create_table
-    sql =  <<-SQL 
+    sql =  <<-SQL
     CREATE TABLE IF NOT EXISTS songs (
-      id INTEGER PRIMARY KEY, 
-      name TEXT, 
+      id INTEGER PRIMARY KEY,
+      name TEXT,
       album TEXT
       )
       SQL

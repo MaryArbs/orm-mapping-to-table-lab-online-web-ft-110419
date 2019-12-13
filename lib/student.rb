f1693df67611b24e503e493936ab5c15e477d9a2
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-12-13 22:06:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,10 @@ class Student
 
   attr_accessor :name, :age
   attr_reader :id
-  def initialize (name, )
+  def initialize (name, age, id = nil )
+    @name = name 
+    @age = age 
+  end 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 

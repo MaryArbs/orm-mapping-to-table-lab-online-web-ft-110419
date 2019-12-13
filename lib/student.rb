9825650a021de033894971815145a6e0b37ef441
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-12-13 22:07:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,9 +2,9 @@ class Student
 
   attr_accessor :name, :grade
   attr_reader :id
-  def initialize (name, age, id = nil )
+  def initialize (name, grade, id = nil )
     @name = name
-    @age = age
+    @grade = grade 
     @id = id
   end
 
